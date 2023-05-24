b493d477539d468b6b66989f4c068ea67093d121
Ulquiorra <rodiontrig@gmail.com>
2023-05-24 21:48:23 +0300

    first

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5 import uic
+import sys
+class App(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.set()
+    def set(self):
+        self.wroot=uic.loadUi('untitled.ui')
+        self.wroot.show()
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    ex=App()
+    app.exec_()
